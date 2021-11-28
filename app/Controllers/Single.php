8ff0b2870e4192bf21e0527e7b83b4055c00b5ef
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2021-11-28 23:41:46 +0100

    Create Single.php

---
+++ app/Controllers/Single.php
@@ -0,0 +1,17 @@
+<?php
+ namespace App\Controllers;
+ use App\Models\ProductsModel;
+ use App\Models\SliderModel;
+ use App\Models\DiscountModel;
+
+
+class Single extends BaseController
+{
+  public function index($id = null){
+    $products = new ProductsModel();
+		$data['single_product'] = $products->find($id);
+    return view('web/pages/single', $data);
+  }
+	//--------------------------------------------------------------------
+
+}
