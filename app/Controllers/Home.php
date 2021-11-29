5536ecd8886230ed3cd59275bd2906f20f52955e
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2021-11-29 22:03:34 +0100

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,32 +9,34 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		$produsts = new ProductsModel();
+		$products = new ProductsModel();
 		$slider = new SliderModel();
 		$discount = new DiscountModel();
 
 
 		$data['slides'] = $slider->where('publication_status', '1')->findAll();
-		$data['all_products'] = $produsts->where('publication_status', '1')->findAll();
-		$data['new_products'] = $produsts->where('publication_status', '1')->orderBy('id', 'DESC')->findAll();
-		$data['total_men'] = count($produsts->where('product_category', '1')->where('publication_status', '1')->findAll());
-		$data['total_women'] = count($produsts->where('product_category', '2')->where('publication_status', '1')->findAll());
-		$data['total_kids'] = count($produsts->where('product_category', '3')->where('publication_status', '1')->findAll());
-		$data['total_accessories'] = count($produsts->where('product_category', '4')->where('publication_status', '1')->findAll());
-		$data['total_cosmetics'] = count($produsts->where('product_category', '5')->where('publication_status', '1')->findAll());
-		$data['featured_products'] = $produsts->where('product_feature', '1')->where('publication_status', '1')->orderBy('id', 'DESC')->limit(5, 1)->findAll();
+		$data['all_products'] = $products->where('publication_status', '1')->findAll();
+		$data['new_products'] = $products->where('publication_status', '1')->orderBy('id', 'DESC')->findAll();
+		$data['total_men'] = count($products->where('product_category', '1')->where('publication_status', '1')->findAll());
+		$data['total_women'] = count($products->where('product_category', '2')->where('publication_status', '1')->findAll());
+		$data['total_kids'] = count($products->where('product_category', '3')->where('publication_status', '1')->findAll());
+		$data['total_accessories'] = count($products->where('product_category', '4')->where('publication_status', '1')->findAll());
+		$data['total_cosmetics'] = count($products->where('product_category', '5')->where('publication_status', '1')->findAll());
+		$data['featured_products'] = $products->where('product_feature', '1')->where('publication_status', '1')->orderBy('id', 'DESC')->limit(5, 1)->findAll();
 		$data['discount'] = $discount->where('discount_status', '1')->findAll();
 		return view('web/pages/home', $data );
 	}
 
-  public function single($product_id=null){
-    $produsts = new ProductsModel();
-		$data['single_product'] = $produsts->find($product_id);
-    return view('web/pages/single', $data);
+  public function Category($id){
+    $products = new ProductsModel();
+    //$category = $products->findAll();
+
+    $data = [];    
+
+    return view('web/pages/category', $data);
   }
 
   public function ShopCart(){
-
     return view('web/pages/shop-cart');
   }
 
