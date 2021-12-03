9c63a32ca0b28f3594b3fc7bea30b1bca61e8a7e
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2021-12-03 23:00:57 +0100

    Create Categories.php

---
+++ app/Controllers/Categories.php
@@ -0,0 +1,14 @@
+<?php
+ namespace App\Controllers;
+
+
+class Categories extends BaseController
+{
+	public function index()
+	{
+		return view('admin/pages/category');
+	}  
+
+	//--------------------------------------------------------------------
+
+}
