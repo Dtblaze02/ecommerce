d86d49c1c42df66a9495809f3b11bbd60006a616
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2021-12-03 23:01:07 +0100

    Create Dashboard.php

---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,32 @@
+<?php
+ namespace App\Controllers;
+ use App\Models\ProductsModel;
+ use App\Models\SliderModel;
+ use App\Models\DiscountModel;
+
+
+class Dashboard extends BaseController
+{
+	public function index()
+	{
+		$produsts = new ProductsModel();
+		$slider = new SliderModel();
+		$discount = new DiscountModel();
+
+
+		$data['slides'] = $slider->where('publication_status', '1')->findAll();
+		$data['all_products'] = $produsts->where('publication_status', '1')->findAll();
+		$data['new_products'] = $produsts->where('publication_status', '1')->orderBy('id', 'DESC')->findAll();
+		$data['total_men'] = count($produsts->where('product_category', '1')->where('publication_status', '1')->findAll());
+		$data['total_women'] = count($produsts->where('product_category', '2')->where('publication_status', '1')->findAll());
+		$data['total_kids'] = count($produsts->where('product_category', '3')->where('publication_status', '1')->findAll());
+		$data['total_accessories'] = count($produsts->where('product_category', '4')->where('publication_status', '1')->findAll());
+		$data['total_cosmetics'] = count($produsts->where('product_category', '5')->where('publication_status', '1')->findAll());
+		$data['featured_products'] = $produsts->where('product_feature', '1')->where('publication_status', '1')->orderBy('id', 'DESC')->limit(5, 1)->findAll();
+		$data['discount'] = $discount->where('discount_status', '1')->findAll();
+		return view('admin/pages/home', $data );
+	}
+
+	//--------------------------------------------------------------------
+
+}
